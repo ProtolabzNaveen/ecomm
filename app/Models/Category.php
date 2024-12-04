bb04a0f14a9952601009cf8737f94dc148d935bc
Naveen <protolabznaveenphp@gmail.com>
2024-12-04 11:33:13 +0530

    manage relationship category and product

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use app\Modles\Product;
 class Category extends Model
 {
     protected $table = 'categories';
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Category;
 class Product extends Model
 {
     protected $table = 'products';
+
+    public function Category(){
+
+        return $this->hasOne(Category::class);
+    }
 
 }
