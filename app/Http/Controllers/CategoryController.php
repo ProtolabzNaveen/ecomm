ce0047b0f175e927f75f6c58b68bcf6f14d86cba
Naveen <protolabznaveenphp@gmail.com>
2024-12-04 10:52:39 +0530

    category added functionality

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,10 +1,53 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
 
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+
+        $categories = Category::paginate(10);
+
+        return response()->json([
+            'success' => true,
+            'data' => $categories,
+        ]);
+    }
+
+    public function store(Request $request){
+         
+        $validator = Validator::make($request->all(),[
+           'name'=> 'required:string',
+           'description'=> 'required|string',
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'success'=>false,
+                 'message'=> 'validaton error',
+                 'errors'=> $validator->errors(),
+            ],422);
+        }
+
+        try{
+
+            $Category = Category::create($request->al());
+            return response()->json([
+               'success'=> true,
+                'message' => 'category  added successfully',
+                 'category' => $Category
+            ],201);
+        }
+        catch(Exception $error){
+            
+            return response()->json([
+               'error'=> $error->messages(),
+               'success' => false,
+            ],500);
+        }
+    }
 }
